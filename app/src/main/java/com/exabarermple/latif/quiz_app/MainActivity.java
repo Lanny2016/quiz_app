6e6fdfcb9c98fc0023ff8ed6f5284099c601f634
latif <latif.hamid@cutesafe.com>
2019-05-28 09:05:32 +0300

    Switch statement for RadioButtons, editText, and checkBoxes but doest work well when Radio button work, editText does not work properly

--- app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
+++ app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
@@ -27,8 +27,76 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                //  calling for editText input
+                groupOne = (RadioGroup) findViewById(R.id.group_one );
+                groupTwo = (RadioGroup) findViewById(R.id.group_two );
+                groupThree = (RadioGroup)findViewById ( R.id.group_three );
+
+                switch (groupOne.getCheckedRadioButtonId())
+                {
+                    case R.id.rb2:
+                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
+
+                        break;
+
+                    case R.id.rb1:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                    case R.id.rb3:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                }
+                // for the second question
+                switch (groupTwo.getCheckedRadioButtonId())
+                {
+                    case R.id.rb4:
+                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
+
+                        break;
+
+                    case R.id.rb5:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                    case R.id.rb6:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                }
+                // for the third question
+                switch (groupThree.getCheckedRadioButtonId())
+                {
+                    case R.id.rb7:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+
+                        break;
+
+                    case R.id.rb8:
+                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                    case R.id.rb9:
+                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
+                        break;
+                }
+                //  calling input method  /////
                 input(v);
+
+                //  for the checkboxes
+                one = (CheckBox)findViewById ( R.id.checkbox_true );
+                two = (CheckBox)findViewById ( R.id.checkbox_long );
+                three = (CheckBox)findViewById ( R.id.checkbox_static );
+                four = (CheckBox)findViewById ( R.id.checkbox_end);
+
+                if (one.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "Answer one" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(two.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "Answer two" , Toast.LENGTH_SHORT ).show ();
+                }
+                if (three.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "Answer three" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(four.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "Answer four" , Toast.LENGTH_SHORT ).show ();
+                }
+
 
             }
 
