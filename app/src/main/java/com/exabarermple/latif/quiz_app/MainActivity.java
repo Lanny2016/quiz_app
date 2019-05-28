5d99117393972606a04faf22af9d26d0074e7ac7
latif <latif.hamid@cutesafe.com>
2019-05-28 09:19:27 +0300

    if statement for RadioButtons, editText, and checkBoxes but doest work well when Radio button work, editText does not work properly

--- app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
+++ app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
@@ -12,108 +12,97 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    RadioGroup groupOne,groupTwo,groupThree;
-   EditText input;
-   CheckBox one,two,three,four;
+    RadioGroup groupOne, groupTwo, groupThree;
+    RadioButton rbOne,rbTwo,rbThree,rbFour,rbFive,rbSix,rbSeven,rbEight,rbNine;
+    EditText input;
+    CheckBox one, two, three, four;
     Button sbm, rst;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
 
-        sbm = (Button)findViewById ( R.id.submitButton );
-
+        sbm = (Button) findViewById ( R.id.submitButton );
         sbm.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
+                rbOne = (RadioButton)findViewById ( R.id.rb1 );
+                rbTwo = (RadioButton)findViewById ( R.id.rb2 );
+                rbThree = (RadioButton)findViewById ( R.id.rb3 );
+                rbFour = (RadioButton)findViewById ( R.id.rb4 );
+                rbFive = (RadioButton)findViewById ( R.id.rb5 );
+                rbSix = (RadioButton)findViewById ( R.id.rb6 );
+                rbSeven = (RadioButton)findViewById ( R.id.rb7 );
+                rbEight = (RadioButton)findViewById ( R.id.rb8 );
+                rbNine = (RadioButton)findViewById ( R.id.rb9 );
 
-                groupOne = (RadioGroup) findViewById(R.id.group_one );
-                groupTwo = (RadioGroup) findViewById(R.id.group_two );
-                groupThree = (RadioGroup)findViewById ( R.id.group_three );
-
-                switch (groupOne.getCheckedRadioButtonId())
-                {
-                    case R.id.rb2:
-                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
-
-                        break;
-
-                    case R.id.rb1:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
-                    case R.id.rb3:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
+                if(rbOne.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "main" , Toast.LENGTH_SHORT ).show ();
                 }
-                // for the second question
-                switch (groupTwo.getCheckedRadioButtonId())
-                {
-                    case R.id.rb4:
-                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
-
-                        break;
-
-                    case R.id.rb5:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
-                    case R.id.rb6:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
+                if(rbTwo.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "implements" , Toast.LENGTH_SHORT ).show ();
                 }
-                // for the third question
-                switch (groupThree.getCheckedRadioButtonId())
-                {
-                    case R.id.rb7:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-
-                        break;
-
-                    case R.id.rb8:
-                        Toast.makeText ( MainActivity.this, "Wrong answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
-                    case R.id.rb9:
-                        Toast.makeText ( MainActivity.this, "Correct answer" , Toast.LENGTH_SHORT ).show ();
-                        break;
+                if(rbThree.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "extends" , Toast.LENGTH_SHORT ).show ();
                 }
+                if(rbFour.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "java.lang" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(rbFive.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "java.awt" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(rbSix.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "java.awt.font" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(rbSeven.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "extend a class" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(rbEight.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "instantiate an object" , Toast.LENGTH_SHORT ).show ();
+                }
+                if(rbNine.isChecked ()){
+                    Toast.makeText ( MainActivity.this, "make objects" , Toast.LENGTH_SHORT ).show ();
+                }
+
                 //  calling input method  /////
-                input(v);
+              
 
                 //  for the checkboxes
-                one = (CheckBox)findViewById ( R.id.checkbox_true );
-                two = (CheckBox)findViewById ( R.id.checkbox_long );
-                three = (CheckBox)findViewById ( R.id.checkbox_static );
-                four = (CheckBox)findViewById ( R.id.checkbox_end);
+                one = (CheckBox) findViewById ( R.id.checkbox_true );
+                two = (CheckBox) findViewById ( R.id.checkbox_long );
+                three = (CheckBox) findViewById ( R.id.checkbox_static );
+                four = (CheckBox) findViewById ( R.id.checkbox_end );
 
-                if (one.isChecked ()){
-                    Toast.makeText ( MainActivity.this, "Answer one" , Toast.LENGTH_SHORT ).show ();
+                if (one.isChecked ()) {
+                    Toast.makeText ( MainActivity.this, "Answer one", Toast.LENGTH_SHORT ).show ();
                 }
-                if(two.isChecked ()){
-                    Toast.makeText ( MainActivity.this, "Answer two" , Toast.LENGTH_SHORT ).show ();
+                if (two.isChecked ()) {
+                    Toast.makeText ( MainActivity.this, "Answer two", Toast.LENGTH_SHORT ).show ();
                 }
-                if (three.isChecked ()){
-                    Toast.makeText ( MainActivity.this, "Answer three" , Toast.LENGTH_SHORT ).show ();
+                if (three.isChecked ()) {
+                    Toast.makeText ( MainActivity.this, "Answer three", Toast.LENGTH_SHORT ).show ();
                 }
-                if(four.isChecked ()){
-                    Toast.makeText ( MainActivity.this, "Answer four" , Toast.LENGTH_SHORT ).show ();
+                if (four.isChecked ()) {
+                    Toast.makeText ( MainActivity.this, "Answer four", Toast.LENGTH_SHORT ).show ();
                 }
 
 
             }
 
 
-    });
+        } );
 
 
+    }
 
-}
+    public void input(View view) {
+        // getting value form editText
+        input = (EditText) findViewById ( R.id.editText_input );
+        String answer = input.getText ().toString ();
+        Toast.makeText ( MainActivity.this, answer, Toast.LENGTH_SHORT ).show ();
 
-public void input(View view){
-    // getting value form editText
-    input = (EditText)findViewById ( R.id.editText_input );
-    String answer = input.getText ().toString ();
-    Toast.makeText ( MainActivity.this, answer , Toast.LENGTH_SHORT ).show ();
-
-}
+    }
 
 
 }
