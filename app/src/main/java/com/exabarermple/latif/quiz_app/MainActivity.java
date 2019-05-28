c94a06d3d273967bc1a4f269198854c8b3f21a88
latif <latif.hamid@cutesafe.com>
2019-05-28 09:20:25 +0300

    if statement for RadioButtons, editText, and checkBoxes but doest work well when Radio button work, editText does not work properly

--- app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
+++ app/src/main/java/com/exabarermple/latif/quiz_app/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 //  calling input method  /////
-              
+
 
                 //  for the checkboxes
                 one = (CheckBox) findViewById ( R.id.checkbox_true );
